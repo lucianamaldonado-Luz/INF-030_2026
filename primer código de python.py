99c834c407b1423059c5b868338f06a4afb02372
lucianamaldonado-Luz <luciana.maldonado@ucb.edu.bo>
2026-01-08 12:28:53 -0400

    python

---
+++ primer código de python.py
@@ -0,0 +1,18 @@
+print("Hola, mundo!")
+
+y = 3
+x = 25
+resultado = x + y
+print("Hola Luz, el resultado es: ")
+print(resultado)
+
+print("--------------------------------")
+
+print("Calcular el area de un rectangulo")
+base = 10
+altura = 57
+area_rectangulo = base * altura
+print(area_rectangulo)
+
+print("----------------------------------")
+print(7*8)
